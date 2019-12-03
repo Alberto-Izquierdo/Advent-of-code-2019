cc19e4bb7cf20ac49bcc0e061806e224be099d78
Alberto-Izquierdo <alberto.izquierdo.ramirez@gmail.com>
2019-12-03 20:46:22 +0100

    Day 3, puzzle 2

--- day_3/src/day_3/Puzzle.java
+++ day_3/src/day_3/Puzzle.java
@@ -5,9 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 public class Puzzle {
 
@@ -27,16 +25,15 @@ public class Puzzle {
 			System.out.println("Error reading file \"input\"");
 			return;
 		}
-		Map<Integer, Set<Integer>> path1 = getMapPositionsVisited(lines[0]);
+		Map<Integer, Map<Integer, Integer>> path1 = getMapPositionsVisited(lines[0]);
 		Integer[] result = getClosestIntersectionToCenter(path1, lines[1]);
-		int distance = (result[0] >= 0 ? result[0] : -result[0]) + (result[1] >= 0 ? result[1] : -result[1]);
 		System.out.println(
-				"Closest intersection to center is = " + result[0] + ", " + result[1] + ", distance = " + distance);
+				"Closest intersection to center is = " + result[0] + ", " + result[1] + ", distance = " + result[2]);
 
 	}
 
-	private static Map<Integer, Set<Integer>> getMapPositionsVisited(String line) {
-		Map<Integer, Set<Integer>> result = new HashMap<Integer, Set<Integer>>();
+	private static Map<Integer, Map<Integer, Integer>> getMapPositionsVisited(String line) {
+		Map<Integer, Map<Integer, Integer>> result = new HashMap<Integer, Map<Integer, Integer>>();
 		Map<Character, Integer[]> dimensions = new HashMap<Character, Integer[]>();
 		dimensions.put('U', new Integer[] { 0, 1 });
 		dimensions.put('D', new Integer[] { 0, -1 });
@@ -44,6 +41,7 @@ public class Puzzle {
 		dimensions.put('L', new Integer[] { -1, 0 });
 		String[] tokens = line.split(",");
 		Integer[] currentPosition = new Integer[] { 0, 0 };
+		int steps = 1;
 		for (String direction : tokens) {
 			Integer[] currentDirection = dimensions.get(direction.charAt(0));
 			Integer distance = Integer.valueOf(direction.substring(1));
@@ -51,21 +49,24 @@ public class Puzzle {
 				currentPosition[0] += currentDirection[0];
 				currentPosition[1] += currentDirection[1];
 				if (result.containsKey(currentPosition[0])) {
-					result.get(currentPosition[0]).add(currentPosition[1]);
+					Map<Integer, Integer> result2 = result.get(currentPosition[0]);
+					if (!result2.containsKey(currentPosition[1])) {
+						result2.put(currentPosition[1], steps);
+					}
 				} else {
-					Set<Integer> list = new HashSet<Integer>();
-					list.add(currentPosition[1]);
+					Map<Integer, Integer> list = new HashMap<Integer, Integer>();
+					list.put(currentPosition[1], steps);
 					result.put(currentPosition[0], list);
 				}
 				--distance;
+				++steps;
 			}
 		}
 		return result;
 	}
 
-	private static Integer[] getClosestIntersectionToCenter(Map<Integer, Set<Integer>> path1, String line) {
-		Integer[] result = new Integer[] { Integer.MAX_VALUE, Integer.MAX_VALUE };
-		int distanceResult = Integer.MAX_VALUE;
+	private static Integer[] getClosestIntersectionToCenter(Map<Integer, Map<Integer, Integer>> path1, String line) {
+		Integer[] result = new Integer[] { Integer.MAX_VALUE, Integer.MAX_VALUE, Integer.MAX_VALUE };
 
 		Map<Character, Integer[]> dimensions = new HashMap<Character, Integer[]>();
 		dimensions.put('U', new Integer[] { 0, 1 });
@@ -74,6 +75,7 @@ public class Puzzle {
 		dimensions.put('L', new Integer[] { -1, 0 });
 		String[] tokens = line.split(",");
 		Integer[] currentPosition = new Integer[] { 0, 0 };
+		int steps = 1;
 		for (String direction : tokens) {
 			Integer[] currentDirection = dimensions.get(direction.charAt(0));
 			Integer distance = Integer.valueOf(direction.substring(1));
@@ -81,19 +83,18 @@ public class Puzzle {
 				currentPosition[0] += currentDirection[0];
 				currentPosition[1] += currentDirection[1];
 				if (path1.containsKey(currentPosition[0])) {
-					Set<Integer> set = path1.get(currentPosition[0]);
-					if (set.contains(currentPosition[1])) {
-						int newDistance = (currentPosition[0] < 0 ? -currentPosition[0] : currentPosition[0])
-								+ (currentPosition[1] < 0 ? -currentPosition[1] : currentPosition[1]);
-						if (newDistance < distanceResult) {
-							distanceResult = newDistance;
+					Map<Integer, Integer> set = path1.get(currentPosition[0]);
+					if (set.containsKey(currentPosition[1])) {
+						int newDistance = steps + set.get(currentPosition[1]);
+						if (newDistance < result[2]) {
 							result[0] = currentPosition[0];
 							result[1] = currentPosition[1];
+							result[2] = newDistance;
 						}
-
 					}
 				}
 				--distance;
+				++steps;
 			}
 		}
 		return result;
